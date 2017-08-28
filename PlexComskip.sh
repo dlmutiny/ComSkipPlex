ece31776bcc824d291ff99fdf025e94469d534b3
David <dave@FozzieBear>
2017-08-28 11:52:50 -0700

    updates done by Dave

--- PlexComskip.sh
+++ PlexComskip.sh
@@ -3,12 +3,8 @@
 #sleep 10
 sleep `echo $$%30| bc`
 /usr/bin/python /home/plex/PlexComskip/PlexComskip.py "$1"
-#mv *.ts *.m4v
+
 HandBrakeCLI -i "$1" -o "$1".m4v --format av_mp4 --encoder x264 --quality 20 --x264-preset veryfast
-#find /home/plex/DVR/ -name "*.ts" -not -path "*/\.*" -delete
-#find /home/plex/DVR -maxdepth 2 -type f -name "*.ts" -delete 
-#find . -type f -name '*.ts' -delete
+
 rm -f "$1"
 
-
-#find /home/plex/DVR -name "*.ts" -delete
